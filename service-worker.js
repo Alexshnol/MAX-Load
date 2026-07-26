2660c4f345842e5561733a8b372ebe75617a4cf4
Alexshnol <shnol.raam@gmail.com>
2026-07-26 23:43:32 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,30 @@
-const CACHE='max-load-v1';const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'max-load-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-64.png',
+  './icon-192.png',
+  './icon-512.png',
+  './daf.jpg',
+  './iveco.jpg'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
